80186834084b5203f001f3ec0215f7b9694f9456
Markim Shaw <ms79723@gmail.com>
2021-02-27 18:40:26 -0600

    Add basic network requesting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", async (request, response) => {
+  await axios
+    .get("https://search.hpb.com/search/suggest", {
+      params: { query: "christopher moore" },
+    })
+    .then((res) => {
+      console.log(res);
+      response.send(res.data);
+    });
+});
+
+app.listen(6060, () => console.log("Running"));
